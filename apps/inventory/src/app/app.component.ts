bb2ccb43be47f486a3f8b40d40cb01d8a9a96d76
soel <soel.shaikh@techextensor.com>
2024-04-03 12:12:25 +0530

    initial commit

---
+++ apps/inventory/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductListComponent } from '@angular-monorepo/products';
+
+@Component({
+  standalone: true,
+  imports: [ProductListComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'inventory';
+}
